a970d5d992448cad40a4e25b643aaf956e35b207
mitchashby16 <mitchashby16@gmail.com>
2015-10-06 14:56:03 -0600

    finished youtube api challenge

--- ruby/youtube/server.rb
+++ ruby/youtube/server.rb
@@ -1,15 +1,16 @@
 require 'sinatra'
 require 'net/http'
 require 'uri'
-require 'pry'
+require 'json'
 require 'openssl'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 get '/' do
-  query = params['search']
+  query   = params['search'].gsub(" ", ",")
+  results = nil
   if query
     uri = URI.parse("https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{query}&key=AIzaSyD6LCx-xYsvUUfeVUf3TCLQTqQtvAmbGXg&maxResults=3")
-    results = Net::HTTP.get(uri)
+    results = JSON.parse(Net::HTTP.get(uri))
   end
-  erb :search
+  erb :search, :locals => {results: results['items']}
 end
