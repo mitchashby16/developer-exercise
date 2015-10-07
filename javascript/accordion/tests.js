303dd56fd2f7f820a638bd93237e95b58b341932
mitchashby16 <mitchashby16@gmail.com>
2015-10-07 12:53:29 -0600

    backbone displays quotes from github

--- javascript/accordion/tests.js
+++ javascript/accordion/tests.js
@@ -1,8 +1,13 @@
-// $(document).ready(function(){
-//   module("Accordion tests");
-//
-//   test("example test", function() {
-//     ok(true);
-//   });
-//
-// });
+$(document).ready(function(){
+
+  test("the page will start with all descriptions closed", function() {
+    equal($('.description').is(':visible'), false, "Descriptions are not show" );
+  });
+
+  test("toggleLink method will close the closest description div", function() {
+    $("a:first").click();
+    $("a:first").click();
+    equal($('.description').is(':visible'), false, "Description is show" );
+  });
+
+});
