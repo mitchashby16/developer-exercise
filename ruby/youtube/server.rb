57c77b2291ec3d8980a937c0d134de4d84f08bdb
mitchashby16 <mitchashby16@gmail.com>
2015-10-07 18:37:56 -0600

    fixed youtube api bugs

--- ruby/youtube/server.rb
+++ ruby/youtube/server.rb
@@ -6,11 +6,13 @@ require 'openssl'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 get '/' do
-  query   = params['search'].gsub(" ", ",")
+  query   = params['search'].gsub(" ", ",") if params['search']
   results = nil
   if query
     uri = URI.parse("https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{query}&key=AIzaSyD6LCx-xYsvUUfeVUf3TCLQTqQtvAmbGXg&maxResults=3")
     results = JSON.parse(Net::HTTP.get(uri))
+    erb :search, :locals => {results: results['items']}
+  else
+    erb :search, :locals => {results: results}
   end
-  erb :search, :locals => {results: results['items']}
 end
